4c125c5a9898d7dc1eb6e18487b294449d2a2ec0
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-22 16:31:25 +0200

    feat add azeinda column to db

--- database/migrations/2024_05_22_142855_add_company_name_to_trains_table.php
+++ database/migrations/2024_05_22_142855_add_company_name_to_trains_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->string('azienda', 120);
-            $table->timestamps();
+        Schema::table('trains', function (Blueprint $table) {
+            //
         });
     }
 
@@ -27,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trains');
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
     }
 };
