0083662e9a5b1436743024a1e255e09da8e70936
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-22 17:33:34 +0200

    feat: add cards with info

--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($trains as $train)
-        <div>
-            <div>{{ $train->azienda }}</div>
-        </div>
-    @endforeach
+    <h1 class="text-center">I TRENI CHE STANNO PER PARTIRE</h1>
+    <div class="row">
+        @foreach ($trains as $train)
+            <div class="col">
+                <div class="card my-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $train->azienda }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">To: {{ $train->departing_station }}</h6>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">From: {{ $train->arriving_station }}</h6>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
